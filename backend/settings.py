8b6d94a8e5fc6285bb1f08e0112c1b8fa56395b1
shota-nukumizu <author2000.1225@gmail.com>
2022-02-13 15:12:46 +0900

    add static root

--- backend/settings.py
+++ backend/settings.py
@@ -119,6 +119,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
